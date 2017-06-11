dce85a686df605254084315dccb8facd94eab111
Kirill Poddubniy <poddubny.kirill@gmail.com>
2017-06-11 15:56:06 +0300

    Jquery approach

--- src/templater.js
+++ src/templater.js
@@ -9,19 +9,23 @@
     });
   }
 
-  function run($elements, tag, template) {
-    $elements.each(function (index, el) {
-      el.outerHTML = render(template, el);
-    });
+  function run($el, tag, template) {
+    let $elements = $el.find(tag),
+      el;
+
+    if (!$elements.length) {
+      return;
+    }
+
+    el = $elements[0];
+    el.outerHTML = render(template, el);
+    run($el, tag, template);
   }
 
   function findTags($el, tags) {
     Object.keys(tags).map(function (key) {
       if (tags.hasOwnProperty(key)) {
-        let $elements = $el.find(key);
-        if ($elements.length) {
-          run($elements, key, tags[key]);
-        }
+        run($el, key, tags[key]);
       }
     });
   }
@@ -39,8 +43,17 @@
   }
 })(jQuery);
 
+$('.super-block').templater({
+  tags: {
+    'super-child-strong': '<strong>{{html}}</strong>'
+  }
+});
+
 $(document).templater({
-    tags: {
-      'panel': '<div class="panel"><div class="panel-heading">{{heading}}</div><div class="panel-body">{{html}}</div></div>'
-    }
+  tags: {
+    'panel': '<div class="panel"><div class="panel-heading">{{heading}}</div><div class="panel-body">{{html}}</div></div>',
+    'bootstrap_button': '<button class="{{class}}" type="{{type}}" aria-label="{{aria-label}}">{{html}}</button>',
+    'bootstrap_link': '<a class="btn btn--default" href="#" role="button">Some Another Text</a>',
+    'super-child-strong': '<i>{{html}}</i>'
+  }
 });
